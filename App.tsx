5466d4e235809d0206878e539e35f554a6a90979
Paul Ganster <paul@ganster.dev>
2021-02-15 10:58:54 +0100

    Ejected expo repository and re-initialized as a raw react-native project

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
-import Main from "./src/Main";
+import Main from './src/Main';
 import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
-  return (
-      <NavigationContainer>
-          <PaperProvider>
-              <Main/>
-          </PaperProvider>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <PaperProvider>
+                <Main />
+            </PaperProvider>
+        </NavigationContainer>
+    );
 }

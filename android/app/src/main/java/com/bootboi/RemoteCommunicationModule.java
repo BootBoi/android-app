6d35e93d96f0d76bab1a48de3548e2a918c3ff39
Paul Ganster <paul@ganster.dev>
2021-02-15 13:42:57 +0100

    Implemented turn on / off within app via native module

--- android/app/src/main/java/com/bootboi/RemoteCommunicationModule.java
+++ android/app/src/main/java/com/bootboi/RemoteCommunicationModule.java
@@ -3,7 +3,8 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import android.util.Log;
+import com.github.bootboi.WakeOnLan;
+import com.github.bootboi.SshCommand;
 
 public class RemoteCommunicationModule extends ReactContextBaseJavaModule {
     RemoteCommunicationModule(ReactApplicationContext context) {
@@ -12,9 +13,64 @@ public class RemoteCommunicationModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void wakeUp(String macAddress, Promise promise) {
-        Log.d("RemoteCommunicationMod", "waking up " + macAddress);
-        //wol = new WakeOnLan(macAddress)
-        promise.resolve(true);
+        try {
+            WakeOnLan wol = new WakeOnLan(macAddress);
+            wol.wakeUp();
+            promise.resolve(true);
+        } catch (Exception exception) {
+            promise.reject(exception);
+        }
+
+    }
+
+    @ReactMethod
+    public void powerOff(String host, int port, String user, String password, Promise promise) {
+        SshCommand sshCommand = new SshCommand(host, port, user, password);
+        try {
+            String result = sshCommand.powerOff();
+            promise.resolve(result);
+        } catch (Exception exception) {
+            promise.reject(exception);
+        }
+    }
+
+    @ReactMethod
+    public void reboot(String host, int port, String user, String password, Promise promise) {
+        SshCommand sshCommand = new SshCommand(host, port, user, password);
+        try {
+            String result = sshCommand.reboot();
+            promise.resolve(result);
+        } catch (Exception exception) {
+            promise.reject(exception);
+        }
+    }
+
+    @ReactMethod
+    public void whoAmI(String host, int port, String user, String password, Promise promise) {
+        SshCommand sshCommand = new SshCommand(host, port, user, password);
+        try {
+            String result = sshCommand.whoAmI();
+            promise.resolve(result);
+        } catch (Exception exception) {
+            promise.reject(exception);
+        }
+    }
+
+    @ReactMethod
+    public void canExecuteAsRoot(String host, int port, String user, String password, Promise promise) {
+        SshCommand sshCommand = new SshCommand(host, port, user, password);
+        try {
+            Boolean result = sshCommand.canExecuteAsRoot();
+            promise.resolve(result);
+        } catch (Exception exception) {
+            promise.reject(exception);
+        }
+    }
+
+    @ReactMethod
+    public void isReachable(String host, int port, String user, String password, Promise promise) {
+        SshCommand sshCommand = new SshCommand(host, port, user, password);
+        promise.resolve(sshCommand.isReachable());
     }
 
     @Override
--- src/Connection/ConnectionDetail.tsx
+++ src/Connection/ConnectionDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NativeModules, StyleSheet, View} from 'react-native';
 import {
   StackNavigationOptions,
@@ -7,7 +7,15 @@ import {
 import {RootStackParamList} from '../Main';
 import {RouteProp} from '@react-navigation/native';
 import LargeButton from '../Component/LargeButton';
-import {DefaultTheme} from 'react-native-paper';
+import {
+  Card,
+  DefaultTheme,
+  Paragraph,
+  Title,
+  ActivityIndicator,
+  Chip,
+} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 type DetailScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -25,9 +33,33 @@ export const ConnectionDetailSetTitle = ({
 }: Props): StackNavigationOptions => ({title: route.params.name});
 
 export default function ConnectionDetail(props: Props) {
+  const [canExecuteAsRoot, setCanExecuteAsRoot] = useState<boolean>(false);
+  const [canExecuteError, setCanExecuteError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const {route, navigation} = props;
   const connection = route.params;
   const {RemoteCommunicationModule} = NativeModules;
+
+  useEffect(() => {
+    const {sshConnection} = connection;
+    if (!sshConnection) {
+      setLoading(false);
+      return;
+    }
+    setLoading(true);
+    const {domain, port, username, password} = sshConnection;
+    RemoteCommunicationModule.canExecuteAsRoot(domain, port, username, password)
+      .then((result: any) => {
+        setCanExecuteError(null);
+        setCanExecuteAsRoot(result);
+      })
+      .catch((error: any) => {
+        setCanExecuteError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   const onTurnOn = () => {
     console.log('clicked turn on');
@@ -39,18 +71,86 @@ export default function ConnectionDetail(props: Props) {
         console.error('oh no', error);
       });
   };
-  const onTurnOff = () => {};
+  const onTurnOff = () => {
+    const {sshConnection} = connection;
+    if (!sshConnection) {
+      return;
+    }
+    const {domain, port, username, password} = sshConnection;
+    RemoteCommunicationModule.powerOff(domain, port, username, password)
+      .then((result: any) => {
+        console.log('great success', result);
+      })
+      .catch((error: any) => {
+        console.error('oh no', error);
+      });
+  };
+  const onReboot = () => {
+    const {sshConnection} = connection;
+    if (!sshConnection) {
+      return;
+    }
+    const {domain, port, username, password} = sshConnection;
+    RemoteCommunicationModule.reboot(domain, port, username, password)
+      .then((result: any) => {
+        console.log('great success', result);
+      })
+      .catch((error: any) => {
+        console.error('oh no', error);
+      });
+  };
+
   const onConfigure = () => {
     navigation.navigate('edit', connection);
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.connectionDetail}>
+        <Card>
+          <Card.Content>
+            <Title>{connection.name}</Title>
+            {connection.lanConnection && (
+              <Paragraph>{connection.lanConnection.macAddress}</Paragraph>
+            )}
+            {loading && <ActivityIndicator animating={true} />}
+            {!loading && (
+              <>
+                {connection.sshConnection && (
+                  <>
+                    <Paragraph>
+                      {connection.sshConnection.username}@
+                      {connection.sshConnection.domain}
+                    </Paragraph>
+
+                    {canExecuteAsRoot ? (
+                      <Paragraph style={styles.successText}>
+                        <Icon name="check" /> Logged with sudo
+                      </Paragraph>
+                    ) : (
+                      <Paragraph style={styles.errorText}>
+                        <Icon name="times" /> Log in failed
+                        {canExecuteError && `: ${canExecuteError}`}
+                      </Paragraph>
+                    )}
+                  </>
+                )}
+              </>
+            )}
+          </Card.Content>
+        </Card>
+      </View>
       <LargeButton onClick={onTurnOn} color="#35bf5c" icon="play-circle">
         Turn On
       </LargeButton>
       <LargeButton onClick={onTurnOff} color="#ea4335" icon="power-off">
         Turn Off
+      </LargeButton>
+      <LargeButton
+        onClick={onReboot}
+        color={DefaultTheme.colors.accent}
+        icon="sync">
+        Reboot
       </LargeButton>
       <LargeButton
         onClick={onConfigure}
@@ -66,5 +166,14 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     padding: 8,
+  },
+  connectionDetail: {
+    padding: 8,
+  },
+  successText: {
+    color: '#35bf5c',
+  },
+  errorText: {
+    color: '#ea4335',
   },
 });
